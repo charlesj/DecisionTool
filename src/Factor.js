c2f0b1a6bcf69247e1f3e5de24250c2b70515e8f
Josh Charles <josh.charles@gmail.com>
2019-03-17 13:22:57 -0700

    Wired up factor operations.

--- src/Factor.js
+++ src/Factor.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import Flexbox from 'flexbox-react'
 
 export const Factor = ({ factor, decision, operations }) => {
-  return (<div>{factor.get('name')}</div>)
+  const factorId = factor.get('factorId')
+  const changeName = (e) => operations.updateFactorName(factorId, e.target.value)
+  const changeWeight = (e) => operations.updateFactorWeight(factorId, e.target.value)
+  return (<Flexbox flexDirection='row'>
+    <input
+      type='text'
+      value={factor.get('name', '')}
+      onChange={changeName}
+    />
+    <strong>Weight:</strong>
+    <input
+      type='text'
+      value={factor.get('weight', '')}
+      onChange={changeWeight}
+    />
+    <button onClick={() => operations.removeFactor(factorId)}>Remove</button>
+  </Flexbox>)
 }
